9bff7d3b1a8f59f3068e56fc34da9b07e90dd504
pdrumm <pdrumm@nd.edu>
2016-02-24 17:01:06 -0500

    Made lots of fxns. 1. randomly generate tiles w/ matching inner colors 2. Frame constructor with options to a. randomly generate frame b. generate solvable frame given the tiles inputted 3. determine if the specific pattern and orientation of tiles provided in the given frame is a valid solution

--- main.js
+++ main.js
@@ -17,11 +17,28 @@ for (i = 0 ; i < colors.length ; i++) {
 }
 
 // Frame Constructor
-var Frame = function(size) {
-    return {
-        left: 'b',
-        right: 'r',
-        pairs: ['gb','gb','bg','bg']
+var Frame = function(size,tiles) {
+    var i, my_pairs = [];
+    if(tiles.length!==0) {
+        for( i=0; i<size; i++ ){
+            my_pairs.push(tiles[i].colors[1]+tiles[i].colors[3]);
+        }
+        return {
+            left: tiles[0].colors[0],
+            right: tiles[tiles.length-1].colors[2],
+            pairs: my_pairs
+        }
+    } else {
+        var pair = "";
+        for( i=0; i<size; i++ ){
+            pair = colors[Math.floor((Math.random() * colors.length))]+colors[Math.floor((Math.random() * colors.length))];
+            my_pairs.push(pair);
+        }
+        return {
+            left: colors[Math.floor((Math.random() * colors.length))],
+            right: colors[Math.floor((Math.random() * colors.length))],
+            pairs: my_pairs
+        }
     }
 };
 
@@ -31,16 +48,27 @@ var Tile = function(cleft, ctop, cright, cbot) {
         colors: [cleft, ctop, cright, cbot]
     }
 };
-
-// Generate Frame
-my_frame = Frame(4);
-
 // Generate Tiles
-var tile1 = Tile('b','g','b','b');
-var tile2 = Tile('b','g','y','b');
-var tile3 = Tile('y','b','r','g');
-var tile4 = Tile('r','b','r','g');
-var tiles = [tile1, tile2, tile3, tile4];
+function create_tiles(n) {
+    var tiles = [], c = [];
+    var i,j;
+    for( i=0; i<n; i++ ){
+        for( j=0; j<4; j++ ) {
+            if(i>0 && j===0){
+                c.push(tiles[i-1].colors[2]);
+            } else {
+                c.push(colors[Math.floor((Math.random() * colors.length))]);
+            }
+        }
+        tiles.push(Tile(c[0],c[1],c[2],c[3]));
+        c = [];
+    }
+    return tiles;
+}
+var n = 4;
+var tiles = create_tiles(n);
+// Generate Frame
+my_frame = Frame(n,tiles);
 
 /*
  Delta - transition function
@@ -95,7 +123,8 @@ var accept = my_frame.right;
 
 // Test if the frame is accepted!
 var soln = [];
-function solve_path(state,string) {
+function solve_path(state,my_string) {
+    var string = my_string.slice(0);
     if(string.length===0){
         return (state===accept);
     }
@@ -106,8 +135,7 @@ function solve_path(state,string) {
             var transition_taken = string.shift();
             var trans;
             for( trans=0; trans<transitions[state][transition_taken].length; trans++ ){
-                if(solve_path(transitions[state][transition_taken][trans],string.slice(0))){//use slice(0) as a cheat to pass array by value
-                    //console.log(state+"->"+transition_taken+"->"+transitions[state][transition_taken][trans]);
+                if(solve_path(transitions[state][transition_taken][trans],string)){//use slice(0) as a cheat to pass array by value
                     soln.unshift(parent_tile[state][transition_taken][transitions[state][transition_taken][trans]]);
                     return true;
                 }
@@ -124,5 +152,33 @@ function solve_path(state,string) {
 }
 
 // test output
+//function for checking if user is right
+function user_check(frame,tiles) { //inputs: the frame, and tiles array, but this time no reordering and no rotations
+    var n = tiles.length;
+    //check top bottom
+    for(i=0; i<n; i++){
+        if((tiles[i].colors[1]+tiles[i].colors[3])!==frame.pairs[i]) {
+            return false;
+        }
+    }
+    //check frame
+    if((tiles[0].colors[0] !== frame.left) || (tiles[n-1].colors[2] !== frame.right)){
+        return false;
+    }
+    //check side, side
+    for(i=0; i<(n-1); i++){
+        if(tiles[i].colors[2]!==tiles[i+1].colors[0]){
+            return false;
+        }
+    }
+    //all possible checked so return true
+    return true;
+}
+//console.log(transitions);
+//console.log(parent_tile);
 console.log(solve_path(start,my_frame.pairs));
 console.log(soln);
+console.log(tiles);
+console.log(my_frame);
+
+console.log(user_check(my_frame,tiles));
