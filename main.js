d27a765c013ee23126eddedf23fb3ea849e13a27
pdrumm <pdrumm@nd.edu>
2016-02-23 23:30:56 -0500

    Cleaned up comments/test code/todo

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 // TODO:
 /*
-1. SUGGESTION under Tile constructor
-2. IDEA under transitions def.
-3. UNDEFINED: figure out the undefined error
+1. IDEA under transitions def.
  */
 
 /* Machine M */
@@ -35,16 +33,6 @@ var Frame = function(size) {
 // Tile Constructor
 var Tile = function(cleft, ctop, cright, cbot) {
     return {
-        /*
-        SUGGESTION: have an array of 4 colors rather than a
-        variable for each, b/c the position is arbitrary.
-        ie. We can always rotate tiles.
-        I added the colors array, so just get rid of the top 4 vars?
-         */
-        left: cleft,
-        right: cright,
-        top: ctop,
-        bot: cbot,
         colors: [cleft, ctop, cright, cbot]
     }
 };
@@ -53,7 +41,6 @@ var Tile = function(cleft, ctop, cright, cbot) {
 my_frame = Frame(4);
 
 // Generate Tiles
-
 var tile1 = Tile('b','g','b','b');
 var tile2 = Tile('b','g','y','b');
 var tile3 = Tile('y','b','r','g');
@@ -63,7 +50,7 @@ var tiles = [tile1, tile2, tile3, tile4];
 // Delta - transition function
 // Transition will be (top,bottom)
 // Dictionary of dictionaries ==> key: current state, inner_key: color_pair / state transition (top,bottom), value: next state
-var transitions = {
+var transitions = {};
     /*IDEA: the machine M we built for the homework only tells IF we solved
     the puzzle or not. It didn't tell us the pieces we used to solve it (which
     could vary if we're allowing the solution to use a puzzle piece more than
@@ -71,17 +58,6 @@ var transitions = {
     orientation?) used to generate each state. Then, we could associate the
     winning transitions with the specific puzzle pieces used.
     */
-
-};
-// testing dictionary truthiness
-/*if(transitions.b){console.log("b is true");
-}else{console.log("b is false");}
-if(transitions.c){console.log("c is true");
-}else{console.log("c is false");}
-if(transitions.b.bb){console.log("b.bb is true");
-}else{console.log("b.bb is false");}
-if(transitions.b.cc){console.log("b.cc is true");
-}else{console.log("b.cc is false");}*/
 var tile_count = tiles.length;
 var tile_seg_count = tiles[0].colors.length;
 for( i=0; i<tile_count; i++ ){
@@ -104,15 +80,7 @@ for( i=0; i<tile_count; i++ ){
         }
     }
 }
-//TESTING adding to dict via variables
-/*var x = 'c';
-var y = 'bb';
-transitions.b.bg = 'b';
-transitions[x] = {};
-transitions[x][y] = 'x';
-transitions[x]['zz'] = '2';
-*/
-console.log(transitions);
+
 // Start State
 var start = my_frame.left;
 
